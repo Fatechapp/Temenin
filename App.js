4b5fce23b160fe8394adea4660218c47341a214c
Fatecha Rizqi Putra Prasetyo <minecream10@gmail.com>
2024-05-10 21:33:02 +0700

    commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image, TouchableOpacity, StyleSheet, Text, View, Button } from 'react-native';
-import LogoPage from './screens/logoPage';
+// import LogoPage from './screens/logoPage';
+import LoginPage from './screens/loginPage';
 
 export default function App() {
   return (
-    <LogoPage/>
+    // <LogoPage/>
+    <LoginPage/>
   );
 }
--- screens/loginPage.js
+++ screens/loginPage.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 export default function LoginPage() {
     return(
-        <View>
+        <View style={styles.container}>
             <Text>Selamat Datang!</Text>
             <Text>Silahkan login terlebih dahulu</Text>
             <View>
@@ -30,6 +30,10 @@ export default function LoginPage() {
     )
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: '#283C57'
+    }
 })
